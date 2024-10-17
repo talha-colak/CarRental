272d31f4105b6ac5182cb76d8c227b7c8c6634a3
Talha Çolak <talha-colak@hotmail.com>
2024-10-17 11:43:00 +0300

    Init commit

--- src/main/java/com/talhacolak/carrental/CarRentalApplication.java
+++ src/main/java/com/talhacolak/carrental/CarRentalApplication.java
@@ -4,16 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class CarRentalApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CarRentalApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login Screen");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.DECORATED);
+        scene.getWindow().centerOnScreen();
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/talhacolak/carrental/LoginController.java
+++ src/main/java/com/talhacolak/carrental/LoginController.java
@@ -1,14 +1,40 @@
 package com.talhacolak.carrental;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+/*import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+*/import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+
+    /* @FXML
+     private Label errorLabel;
+
+       public void validateLogin() {
+           if (loginFailed) {
+               errorLabel.setText("Geçersiz Kullanıcı Adı ve Parola");
+           }
+
+       }
+       */
     @FXML
-    private Label welcomeText;
+    private Button goNext;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void goNext() throws IOException {
+        Stage stage = (Stage) goNext.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(CarRentalApplication.class.getResource("dashboard.fxml"));
+        Parent scene = loader.load();
+        stage.setScene(new Scene(scene));
+        stage.setTitle("Dashboard");
+        stage.getScene().getWindow().centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
     }
 }
